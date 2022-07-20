090528bf8a1594b506d1f507a7df33ef681b432a
abhinavrau <abhinav.rau@gmail.com>
2022-07-20 15:00:27 +0000

    Update messsage for KeyBank

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -14,16 +14,16 @@
 
 
 locals {
-  network = "${element(split("-", var.subnet), 0)}"
+  network = element(split("-", var.subnet), 0)
 }
 
 resource "google_compute_instance" "http_server" {
-  project      = "${var.project}"
+  project      = var.project
   zone         = "us-west1-a"
   name         = "${local.network}-apache2-instance"
   machine_type = "f1-micro"
 
-  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Environment: ${local.network}</h1><h2>Hello SouthEast team! Raj and Anthony </h2></body></html>' | sudo tee /var/www/html/index.html"
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Environment: ${local.network}</h1><h2>Hello KeyBank! </h2></body></html>' | sudo tee /var/www/html/index.html"
 
   boot_disk {
     initialize_params {
@@ -32,7 +32,7 @@ resource "google_compute_instance" "http_server" {
   }
 
   network_interface {
-    subnetwork = "${var.subnet}"
+    subnetwork = var.subnet
 
     access_config {
       # Include this section to give the VM an external ip address
